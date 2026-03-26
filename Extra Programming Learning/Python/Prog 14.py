7513081dd34f31eb62f006288653c0a287963f5e
Malcolm Cephas <137897091+malcolm-cephas@users.noreply.github.com>
2026-03-26 15:55:13 +0530

    Delete Extra Programming Learning directory

--- Extra Programming Learning/Python/Prog 14.py
+++
@@ -1,6 +0,0 @@
-'''Write a python program to define a module to find Fibonacci Numbers and import the
-module to another program'''
-#import fibonacci module
-import fibonacci 
-num=int(input("Enter any number to print Fibonacci series "))
-fibonacci.fib(num)
--- Extra Programming Learning/Python/fibonacci.py
+++
@@ -1,5 +0,0 @@
-def fib(n):    # write Fibonacci series up to n
-    a, b = 0, 1
-    while b < n:
-        print(b, end =" ")
-        a, b = b, a+b
